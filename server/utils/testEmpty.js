ee055c3288d1a32ff07fe259c6b35c3b5ebba86f
gisonyeung <gisonyeung2015@163.com>
2016-08-17 11:07:52 +0800

    debug empty

--- server/utils/testEmpty.js
+++ server/utils/testEmpty.js
@@ -2,8 +2,8 @@ module.exports = function testEmpry(str) {
 	
 	if ( !/\S/.test(str) // 检测不到非空字符
 		|| str == 'undefined' // 数据为undefined
-		|| !(/[^\u0000-\u0020|^\u007f]/g.test(str)) // 0~32号及127号：控制字符或通讯专用字符
-		|| !(/[\u4E00-\u9FA5]/g.test(str)) // 检测不到中文字符
+		|| !(/[^\u0000-\u0020|^\u007f]/g.test(str)) // 检测不到0~32号及127号以外的字符：控制字符或通讯专用字符
+		|| !(/[\u4E00-\u9FA5|\w]/g.test(str)) // 检测不到中英文字符
 	) {
 		return true;
 	} else {
