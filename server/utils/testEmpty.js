99d2dd9a413d6e127a3256d2162fc885a15db336
gisonyeung <gisonyeung2015@163.com>
2016-08-17 10:48:12 +0800

    debug empty

--- server/utils/testEmpty.js
+++ server/utils/testEmpty.js
@@ -2,7 +2,7 @@ module.exports = function testEmpry(str) {
 	
 	if ( !/\S/.test(str) // 检测不到非空字符
 		|| str == 'undefined' // 数据为undefined
-		|| /[^\u0000-\u0020|^\u007f]/g.test(str) // 0~32号及127号：控制字符或通讯专用字符
+		|| !(/[^\u0000-\u0020|^\u007f]/g.test(str)) // 0~32号及127号：控制字符或通讯专用字符
 		|| !(/[\u4E00-\u9FA5]/g.test(str)) // 检测不到中文字符
 	) {
 		return true;
