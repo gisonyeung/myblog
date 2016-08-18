0f40ba23a4cb94ac29478bc3458e7e744fffae8f
gisonyeung <gisonyeung2015@163.com>
2016-08-18 22:35:09 +0800

    add icp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import SelfInfoBar from './SelfInfoBar';
 import HomeBlog from './HomeBlog';
+import Icp from './Icp';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Home = React.createClass({
@@ -9,17 +10,20 @@ const Home = React.createClass({
 
   render() {
     return (
-      <div className="inner-wrapper">
-        <ReactCSSTransitionGroup
-          transitionName="blog" 
-          transitionAppear={true} 
-          transitionAppearTimeout={400}
-          transitionEnterTimeout={400}
-          transitionLeaveTimeout={300}
-        >
-          <HomeBlog query={this.props.location.query} />
-        </ReactCSSTransitionGroup>
-        <SelfInfoBar />
+      <div>
+        <div className="inner-wrapper">
+          <ReactCSSTransitionGroup
+            transitionName="blog" 
+            transitionAppear={true} 
+            transitionAppearTimeout={400}
+            transitionEnterTimeout={400}
+            transitionLeaveTimeout={300}
+          >
+            <HomeBlog query={this.props.location.query} />
+          </ReactCSSTransitionGroup>
+          <SelfInfoBar />
+        </div>
+        <Icp />
       </div>
     )
   }
---
+++ src/components/Icp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Icp = React.createClass({
+
+
+  render() {
+  	
+    return (
+      <div className="icp">
+        ICP：
+        <a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备16069727号</a>
+      </div>
+    )
+  }
+
+});
+
+export default Icp;
