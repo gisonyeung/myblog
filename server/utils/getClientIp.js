e3b75eafb43ca5d3cfebf3347d1af1524fc6f9a0
gisonyeung <gisonyeung2015@163.com>
2016-08-22 12:16:49 +0800

    update clientIp

--- server/utils/getClientIp.js
+++ server/utils/getClientIp.js
@@ -1,7 +1,15 @@
 module.exports = function getClientIp(req) {
 
-    return req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
+	var address = ''
+
+	try {
+		address = req.headers['x-forwarded-for'] ||
+		    req.connection.remoteAddress ||
+		    req.socket.remoteAddress;
+	} catch(e) {
+		address = '未知IP';
+	}
+
+
+    return address.replace('::ffff:', '');
 };
